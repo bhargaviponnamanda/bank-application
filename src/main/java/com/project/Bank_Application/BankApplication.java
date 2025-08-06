0e0039b98f53d15ccee2d4244aab019361ff947f
Bhargavi <bhargavi.ponnamanda1999@gmail.com>
2025-08-06 10:22:24 +0530

    Swagger documentation  added for API endpoints

--- src/main/java/com/project/Bank_Application/BankApplication.java
+++ src/main/java/com/project/Bank_Application/BankApplication.java
@@ -6,9 +6,36 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication(scanBasePackages = "com.project")
 @EnableJpaRepositories(basePackages = "com.project.repository")
-@EntityScan(basePackages = "com.project.entity") 
+@EntityScan(basePackages = "com.project.entity")
+@OpenAPIDefinition(
+		         info = @Info(
+		        		 title = "Bank Application",
+		                 description = "Backend Rest APIs for Bank App",
+		                 version = "v1.0",
+		                 contact = @Contact(
+		                		 name = "Bhargavi",
+		                		 email = "bhargaviponnamanda1999@gmail.com",
+		                		 url = "https://github.com/bhargaviponnamanda/bank-application"
+		                ),
+		                 license = @License(
+		                		 name = "Bhargavi",
+		                		 url = "https://github.com/bhargaviponnamanda/bank-application"
+		                  )
+		        		),
+		           externalDocs = @ExternalDocumentation(
+		        		   description = "Bank Application",
+		        		   url = "https://github.com/bhargaviponnamanda/bank-application"
+		        		   )
+		         )
+
 public class BankApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -14,25 +14,36 @@ import com.project.dto.TransferRequest;
 import com.project.dto.UserRequest;
 import com.project.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/user/")
+@Tag(name = "User Account Management Api's")
 public class UserController {
 
 	@Autowired
 	UserService service;
-
+	
+	@Operation(summary = "Create New User Account", description = "Creating a new user and assigning an account ID")
+	@ApiResponse(responseCode = "201", description = "Http Status 201 CREATED")
 	@PostMapping("createAccount")
 	public BankResponse createAccount(@RequestBody UserRequest request) {
 		return service.createAccount(request);
 
 	}
 	
+	@Operation(summary = "Name Enquiry", description = "User Account Enquiry based on the name")
+	@ApiResponse(responseCode = "202", description = "Http Status 202 CREATED")
 	@GetMapping("nameEnquiry")
 	public String nameEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
 		return service.nameEnquiry(enquiryRequest);
 
 	}
 	
+	@Operation(summary = "Balance Enquiry", description = "Retriveing User Account details for balance")
+	@ApiResponse(responseCode = "203", description = "Http Status 203 CREATED")
 	@GetMapping("balanceEnquiry")
 	public BankResponse balanceEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
 		return service.balanceEnquiry(enquiryRequest);
